aec4220658ef52df43dabf8ad75474ea03271197
Artcoded <dev@artcoded.net>
2022-02-19 08:32:02 +0100

    graphql schema generation

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,18 @@
+const {join} = require('path')
+
 module.exports = ({ env }) => ({
   // other plugins' configuration
+
+  graphql: {
+    enabled: true,
+    config: {
+      generateArtifacts: true,
+      artifacts: {
+        schema: join(__dirname, '..', 'schema.graphql'),
+        typegen: join(__dirname, '..', 'types.d.ts'),
+      },
+    }
+  },
   email: {
     config: {
       provider: "nodemailer",
